d8d286a65958109ce00039b3750cd9d3d1d93603
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2025-12-20 14:46:07 +0300

    fish config files

---
+++ desktopconf/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g theme_display_hostname yes
+set -g theme_display_user ssh
+set -g theme_color_scheme catppuccin-mocha
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_show_exit_status yes
