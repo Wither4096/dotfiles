0c945979ffa4c1530d08489c4298e446c24dc6d3
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2025-12-20 14:45:15 +0300

    fish config change

--- fish.sh
+++ fish.sh
@@ -2,4 +2,3 @@
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 echo "omf install bobthefish"|fish
-printf "set -g theme_display_hostname yes\n set -g theme_color_scheme dracula" >> ~/.config/fish/config.fish
