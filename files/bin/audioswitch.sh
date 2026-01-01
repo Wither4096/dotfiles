80e7e9bc074747e07240646d4003abe99acc166e
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-01-01 03:28:14 +0300

    i3 & polybar

---
+++ files/bin/audioswitch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if pactl info|grep 'alsa_output.usb-Generic_USB_Audio-00.analog-stereo';
+then pactl set-default-sink alsa_output.pci-0000_01_00.1.hdmi-stereo&paplay /usr/share/sounds/oxygen/stereo/complete-media-burn.ogg;
+else pactl set-default-sink alsa_output.usb-Generic_USB_Audio-00.analog-stereo&paplay /usr/share/sounds/oxygen/stereo/complete-media-burn.ogg;
+fi
