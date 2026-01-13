930df509b0fe3778fca873e89b4ca0f03edcfbb7
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-01-13 17:12:37 +0300

    dunst script audio

--- files/scripts/dunsttoggle.sh
+++ files/scripts/dunsttoggle.sh
@@ -2,7 +2,15 @@
 command -v polybar-msg &> /dev/null 2>&1
 POLYBAR_EXIST=$?
 dunstctl set-paused toggle
+update_polybar(){
+	if dunstctl is-paused -e;then
+		polybar-msg action '#notifications-toggle.hook.0'
+		paplay /usr/share/sounds/oxygen/stereo/game-over-loser.ogg
+	else
+		polybar-msg action '#notifications-toggle.hook.0'
+		paplay /usr/share/sounds/oxygen/stereo/game-over-winner.ogg
+	fi
+}
 if [ $POLYBAR_EXIST -eq 0 ];then
-	polybar-msg action '#notifications-toggle.hook.0';
-	paplay /usr/share/sounds/oxygen/stereo/game-over-winner.ogg
+	update_polybar
 fi
