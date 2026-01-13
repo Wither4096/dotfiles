dbb7ea0697d7bb49e808698d67e1a61e56514994
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-01-13 16:52:30 +0300

    script fix and binds

--- files/scripts/dunsttoggle.sh
+++ files/scripts/dunsttoggle.sh
@@ -1,10 +1,8 @@
 #/bin/bash
-if dunstctl is-paused -e;then
-	dunstctl set-paused toggle
+command -v polybar-msg &> /dev/null 2>&1
+POLYBAR_EXIST=$?
+dunstctl set-paused toggle
+if [ $POLYBAR_EXIST -eq 0 ];then
+	polybar-msg action '#notifications-toggle.hook.0';
 	paplay /usr/share/sounds/oxygen/stereo/game-over-winner.ogg
-	polybar-msg action '#notifications-toggle.hook.0';
-else
-	dunstctl set-paused toggle
-	paplay /usr/share/sounds/oxygen/stereo/game-over-loser.ogg
-	polybar-msg action '#notifications-toggle.hook.0';
 fi
--- files/scripts/mictoggle.sh
+++ files/scripts/mictoggle.sh
@@ -4,11 +4,11 @@ POLYBAR_EXIST=$?
 pamixer --source @DEFAULT_SOURCE@ -t
 update_polybar(){
 	if pamixer --source @DEFAULT_SOURCE@ --get-mute|grep 'true';then 
+		polybar-msg action '#mictoggle.hook.0';
 		paplay /usr/share/sounds/oxygen/stereo/message-contact-in.ogg
-		polybar-msg action '#mictoggle.hook.0';
 	else 
-		paplay /usr/share/sounds/oxygen/stereo/message-contact-out.ogg
 		polybar-msg action '#mictoggle.hook.0';
+		paplay /usr/share/sounds/oxygen/stereo/message-contact-out.ogg
 	fi
 }
 if [ "$POLYBAR_EXIST" -eq 0 ];then 
