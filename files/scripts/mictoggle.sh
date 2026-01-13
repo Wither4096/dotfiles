1caf3e7ee0d78d54bdeac462a0cd6d70712e406c
Wither <35404007+Wither4096@users.noreply.github.com>
2026-01-13 17:40:23 +0300

    Update mictoggle.sh

--- files/scripts/mictoggle.sh
+++ files/scripts/mictoggle.sh
@@ -5,10 +5,10 @@ pamixer --source @DEFAULT_SOURCE@ -t
 update_polybar(){
 	if pamixer --source @DEFAULT_SOURCE@ --get-mute|grep 'true';then 
 		polybar-msg action '#mictoggle.hook.0';
-		paplay /usr/share/sounds/oxygen/stereo/message-contact-in.ogg
+		paplay /usr/share/sounds/oxygen/stereo/message-contact-out.ogg
 	else 
 		polybar-msg action '#mictoggle.hook.0';
-		paplay /usr/share/sounds/oxygen/stereo/message-contact-out.ogg
+		paplay /usr/share/sounds/oxygen/stereo/message-contact-in.ogg
 	fi
 }
 if [ "$POLYBAR_EXIST" -eq 0 ];then 
