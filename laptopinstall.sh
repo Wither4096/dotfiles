70dcae89934f34388d2f84619085da654fde2f2b
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-02-13 21:05:52 +0300

    mic config laptop

--- laptopinstall.sh
+++ laptopinstall.sh
@@ -17,7 +17,7 @@ cp files/.vimrc $HOME
 cp -r files/wallpapers/ $HOME/.config
 cp -R files/.vim/ /root/
 cp files/.vimrc /root/
-sudo cp files/00-lid.conf /etc/systemd/logind.conf.d/
+# sudo cp files/00-lid.conf /etc/systemd/logind.conf.d/
 sudo cp files/20-limits.conf /etc/tlp.d/
 
 sudo cp files/99-noto-reject-nastaliq.conf /etc/fonts/conf.d/
