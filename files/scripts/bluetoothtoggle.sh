cbe9200ea9a44486a20c695efb8ca72a92dd68c0
Wither4096 <35404007+Wither4096@users.noreply.github.com>
2026-04-11 01:02:16 +0300

    conf update

--- files/scripts/bluetoothtoggle.sh
+++ files/scripts/bluetoothtoggle.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
-then
+
+BT_STATE=$(bluetoothctl show | grep "Powered: yes" | wc -c)
+command -v polybar-msg &> /dev/null 2>&1
+POLYBAR_EXIST=$?
+
+update_polybar(){
+	if [ $BT_STATE -eq 0 ];then
+		polybar-msg action "#bluetooth.hook.1"
+	else
+		polybar-msg action "#bluetooth.hook.0"
+	fi
+}
+
+if [ $BT_STATE -eq 0 ];then
   bluetoothctl power on
-  polybar-msg action "#bluetooth.hook.1"
 else
   bluetoothctl power off
-  polybar-msg action "#bluetooth.hook.0"
+fi
+
+if [ $POLYBAR_EXIST -eq 0 ];then
+	update_polybar
 fi
